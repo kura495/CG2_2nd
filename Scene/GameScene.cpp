6971ad4bc2567e56a8867dbf89f3746ce39c6761
kura495 <104417866+kura495@users.noreply.github.com>
2023-07-12 15:13:35 +0900

    完成

--- Scene/GameScene.cpp
+++ Scene/GameScene.cpp
@@ -59,9 +59,9 @@ void GameScene::Draw()
 	
 	myEngine_->Draw(Left[0], Top[0], Right[0], Color[0], camera_->transformationMatrixData, UV);
 	//myEngine_->Draw(Left[1], Top[1], Right[1], Color[0], camera_->transformationMatrixData, UV);
-	myEngine_->DrawSprite(LeftTop[0], LeftBottom[0], RightTop[0], RightBottom[0], Color[0], UV);
-	myEngine_->DrawSphere(sphere, camera_->transformationMatrixData,Color[0], SphereTexture);
-	myEngine_->DrawModel(modelData,{0,0,0}, camera_->transformationMatrixData,Color[0]);
+	//myEngine_->DrawSprite(LeftTop[0], LeftBottom[0], RightTop[0], RightBottom[0], Color[0], UV);
+	//myEngine_->DrawSphere(sphere, camera_->transformationMatrixData,Color[0], SphereTexture);
+	//myEngine_->DrawModel(modelData,{0,0,0}, camera_->transformationMatrixData,Color[0]);
 	//描画ここまで
 	myEngine_->VertexReset();
 	ImGuiManager_->EndFrame();
