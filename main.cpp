ae7847e18e7165c85a2f01093d3bb8878e96c867
kura495 <104417866+kura495@users.noreply.github.com>
2023-07-14 17:42:33 +0900

    出来そう

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include"GameManager.h"
 #include"D3DResourceLeakChecker.h"
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	D3DResourceLeakChecker leakCheck;
 	GameManager* gameManager=new GameManager();
 	gameManager->Initialize();
 	gameManager->Gameloop();
 	return 0;
 }
+
