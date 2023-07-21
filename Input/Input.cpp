628dc6d18802614edde0d529d93baab5419d9280
kura495 <104417866+kura495@users.noreply.github.com>
2023-07-21 16:39:28 +0900

    Input途中

--- Input/Input.cpp
+++ Input/Input.cpp
@@ -3,5 +3,5 @@
 void Input::Initialize(WinApp*winApp_){
 	hr = DirectInput8Create(winApp_->GetHINSTANCE(),DIRECTINPUT_VERSION,IID_IDirectInput8,(void**)&directInput,nullptr);
 	assert(SUCCEEDED(hr));
-	hr=directInput->CreateDevice(GUID_SysKeyboard,&keyboard,NULL)
+	hr = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
 }
