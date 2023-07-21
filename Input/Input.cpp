9f724caf2852ccd24fe4e4d6e5d9de7bb8ea6bac
kura495 <104417866+kura495@users.noreply.github.com>
2023-07-21 17:16:15 +0900

    インプット途中

--- Input/Input.cpp
+++ Input/Input.cpp
@@ -1,7 +1,15 @@
 #include"Input.h"
 
 void Input::Initialize(WinApp*winApp_){
+	//DirectInputのオブジェクトを作成
 	hr = DirectInput8Create(winApp_->GetHINSTANCE(),DIRECTINPUT_VERSION,IID_IDirectInput8,(void**)&directInput,nullptr);
 	assert(SUCCEEDED(hr));
+	//キーボードデバイスを生成
 	hr = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(hr));
+	//入力データ形式のセット
+
+	hr = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(hr));
+
 }
