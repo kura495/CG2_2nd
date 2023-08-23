4b4452bbedddd987d8135cf2a8f6ca95f6d01011
kura495 <104417866+kura495@users.noreply.github.com>
2023-08-24 01:44:06 +0900

    ルートパラメータ―を新しく追加

--- Base/DirectXCommon.cpp
+++ Base/DirectXCommon.cpp
@@ -433,15 +433,21 @@ void DirectXCommon::MakeRootSignature()
 {
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
 	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-	D3D12_ROOT_PARAMETER rootParameters[4] = {};
+	D3D12_ROOT_PARAMETER rootParameters[5] = {};
 	//色に関するルートパラメーター
 	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CSVで使う
 	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//PIXELShaderで使う
 	rootParameters[0].Descriptor.ShaderRegister = 0;//レジスタ番号を0にバインド
-	//頂点位置に関するルートパラメーター
+
+	//頂点位置に関するルートパラメーター(WorldTransform)
 	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CSVで使う
 	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;//VERTEXShaderで使う
 	rootParameters[1].Descriptor.ShaderRegister = 0;//レジスタ番号を0にバインド
+	//頂点位置に関するルートパラメーター(ViewProjection)
+	rootParameters[4].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CSVで使う
+	rootParameters[4].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;//VERTEXShaderで使う
+	rootParameters[4].Descriptor.ShaderRegister = 1;//レジスタ番号を0にバインド
+
 	descriptionRootSignature.pParameters = rootParameters;//ルートパラメータ配列へのポインタ
 	descriptionRootSignature.NumParameters = _countof(rootParameters);//配列の長さ
 	//テクスチャで使う
@@ -458,7 +464,7 @@ void DirectXCommon::MakeRootSignature()
 	rootParameters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange);//Tableで利用する数
 	//ライティングのrootParameters
 	rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CSVで使う
-	rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//VERTEXShaderで使う
+	rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//PixelShaderで使う
 	rootParameters[3].Descriptor.ShaderRegister = 1;//レジスタ番号を1にバインド
 
 	//Samplerの設定
