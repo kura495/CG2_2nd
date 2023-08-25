8c62ef12342b7ae1fa4e2cf9ff5fc7a17ba386c7
kura495 <104417866+kura495@users.noreply.github.com>
2023-08-25 22:52:38 +0900

    WinAppとImGuiManagerをシングルトンに修正

--- Scenes/Manager/GameManager.cpp
+++ Scenes/Manager/GameManager.cpp
@@ -36,8 +36,8 @@ void GameManager::Initialize()
 	//グローバル変数読み込み
 	GlobalVariables::GetInstance()->LoadFiles();
 	//State
-	state[TITLE]=std::make_unique<GameTitleState>();
-	state[PLAY]= std::make_unique<GamePlayState>();
+	state[TITLE]　=　std::make_unique<GameTitleState>();
+	state[PLAY]　= std::make_unique<GamePlayState>();
 	state[PLAY]->Initialize();
 	GameState::StateNo = TITLE;
 }
